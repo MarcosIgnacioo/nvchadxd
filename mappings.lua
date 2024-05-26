ae48b615689ba04753aee520e13c9ac4da11b5f4
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-05-25 20:48:49 -0700

    lol

---
+++ mappings.lua
@@ -0,0 +1,108 @@
+local map = vim.keymap.set
+
+map("i", "<C-b>", "<ESC>^i", { desc = "Move Beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "Move End of line" })
+map("i", "<C-h>", "<Left>", { desc = "Move Left" })
+map("i", "<C-l>", "<Right>", { desc = "Move Right" })
+map("i", "<C-j>", "<Down>", { desc = "Move Down" })
+map("i", "<C-k>", "<Up>", { desc = "Move Up" })
+
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "Switch Window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Switch Window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Switch Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Switch Window up" })
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "File Save" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "File Copy whole" })
+
+map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle Relative number" })
+map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "Toggle NvCheatsheet" })
+
+map("n", "<leader>fm", function()
+    require("conform").format { lsp_fallback = true }
+end, { desc = "Format Files" })
+
+-- global lsp mappings
+map("n", "gl", vim.diagnostic.open_float, { desc = "Lsp floating diagnostics" })
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Lsp prev diagnostic" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Lsp next diagnostic" })
+map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Lsp diagnostic loclist" })
+
+map("n", "<tab>", function()
+    require("nvchad.tabufline").next()
+end, { desc = "Buffer Goto next" })
+
+map("n", "<S-tab>", function()
+    require("nvchad.tabufline").prev()
+end, { desc = "Buffer Goto prev" })
+
+map("n", "<leader>x", function()
+    require("nvchad.tabufline").close_buffer()
+end, { desc = "Buffer Close" })
+
+-- Comment
+map("n", "<leader>/", function()
+    require("Comment.api").toggle.linewise.current()
+end, { desc = "Comment Toggle" })
+
+map(
+    "v",
+    "<leader>/",
+    "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+    { desc = "Comment Toggle" }
+)
+
+-- nvimtree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree Focus window" })
+
+-- telescope
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Telescope Find buffers" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Telescope Help page" })
+
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Telescope Find oldfiles" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Telescope Find in current buffer" })
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "Telescope Git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope Git status" })
+map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "Telescope Pick hidden term" })
+map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "Telescope Nvchad themes" })
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope Find files" })
+map(
+    "n",
+    "<leader>fa",
+    "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+    { desc = "Telescope Find all files" }
+)
+
+-- terminal
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "Terminal Escape terminal mode" })
+
+-- new terminals
+map("n", "<leader>h", function()
+    require("nvchad.term").new { pos = "sp", size = 0.3 }
+end, { desc = "Terminal New horizontal term" })
+
+map("n", "<leader>v", function()
+    require("nvchad.term").new { pos = "vsp", size = 0.3 }
+end, { desc = "Terminal New vertical window" })
+
+-- toggleable
+map({ "n", "t" }, "<A-v>", function()
+    require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm", size = 0.3 }
+end, { desc = "Terminal Toggleable vertical term" })
+
+map({ "n", "t" }, "<A-h>", function()
+    require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm", size = 0.3 }
+end, { desc = "Terminal New horizontal term" })
+
+map({ "n", "t" }, "<A-i>", function()
+    require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "Terminal Toggle Floating term" })
+
+map("t", "<ESC>", function()
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_close(win, true)
+end, { desc = "Terminal Close term in terminal mode" })
---
+++ plugins/ui.lua
@@ -0,0 +1,55 @@
+return {
+
+  {
+    "NvChad/base46",
+    branch = "v2.5",
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  {
+    "NvChad/ui",
+    branch = "v2.5",
+    lazy = false,
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "User FilePost",
+    opts = { user_default_options = { names = false } },
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+
+      -- execute colorizer as soon as possible
+      vim.defer_fn(function()
+        require("colorizer").attach_to_buffer(0)
+      end, 0)
+    end,
+  },
+
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = function()
+      return { override = require "nvchad.icons.devicons" }
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "devicons")
+      require("nvim-web-devicons").setup(opts)
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      return require "nvchad.configs.nvimtree"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "nvimtree")
+      require("nvim-tree").setup(opts)
+    end,
+  },
+}
